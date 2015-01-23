ef9fd0df80e8c4e23c50e910b5aef8cf50414391
Synchronistic Savage <savgeonabike@gmail.com>
2015-01-23 01:41:48 -0800

    added framerate option to the ffmpeg x11 to v4l2 script

--- ffmpeg/ffmpeg-x11-v4l2.sh
+++ ffmpeg/ffmpeg-x11-v4l2.sh
@@ -5,8 +5,8 @@
 VDEV=/dev/video4
 #set the capture area
 CAPZONE=640x480
-
+FPS=30
 mkfifo /tmp/deskpipe
 yuv4mpeg_to_v4l2 /dev/video4 < /tmp/deskpipe &
 
-ffmpeg -f x11grab -follow_mouse centered -r 60 -s $CAPZONE -i :0.0+0,0 -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f yuv4mpegpipe /tmp/deskpipe
+ffmpeg -f x11grab -follow_mouse centered -r $FPS -s $CAPZONE -i :0.0+0,0 -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f yuv4mpegpipe /tmp/deskpipe
